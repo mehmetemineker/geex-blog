8066ad536aa414d1b6e13245d34cc34704e51982
Mehmet Emin EKER <mehmetemineker@outlook.com>
2017-12-06 00:27:55 +0300

    bootstrap 4 alpha added and sqlite included

--- Geex-Blog/App_Start/RouteConfig.cs
+++ Geex-Blog/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace GeexBlog
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Index", action = "Index", id = UrlParameter.Optional }
+                 defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Geex-Blog/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeexBlog.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            DataTable dt = new DataTable();
+
+            using (SQLiteConnector connector = new SQLiteConnector())
+            {
+                dt = connector.LoadData("Select * from posts");
+            }
+
+
+            using (SQLiteConnector connector = new SQLiteConnector())
+            {
+                connector.ExecuteQuery("insert into posts(title,content,created) values('başlık','content','tarih')");
+            }
+
+
+            return View();
+        }
+    }
+}
